c6bebc8b5b6e13c3bfb95299719650b5b2aa716f
matthew.serre <matthew.serre@gmail.com>
2019-05-26 00:19:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,4 +8,4 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = @@items.find{|i| i.name
+      item = @@items.find{|i| i.name == item_name
